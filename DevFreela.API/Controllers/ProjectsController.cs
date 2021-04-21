34bcd976bb4a709eef81d5a7e099e2806040f43b
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-21 20:55:31 -0300

    Creating endpoint for projects

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,45 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{îd}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+    }
+}
---
+++ DevFreela.API/Models/CreateProjectModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Models
+{
+    public class CreateProjectModel
+    {
+        public object Id { get; internal set; }
+    }
+}
