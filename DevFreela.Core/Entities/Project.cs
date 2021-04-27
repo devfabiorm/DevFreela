143df40db9658f41b6edcbcf9021901a07fea1e9
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-26 23:57:14 -0300

    Creating domain layer and entities of the project

---
+++ DevFreela.Core/Entities/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class Project : BaseEntity
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int ClientId { get; set; }
+        public int FreelancerId { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
