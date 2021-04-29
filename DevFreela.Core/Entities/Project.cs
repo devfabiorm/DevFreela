09f46803dd53a43f72d0206d7047283d4e855d3a
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-28 23:00:26 -0300

    Finishing creation of project's entities

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -1,11 +1,34 @@
-﻿namespace DevFreela.Core.Entities
+﻿using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace DevFreela.Core.Entities
 {
     public class Project : BaseEntity
     {
+        public Project(string title, string description, int clientId, int freelancerId, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            ClientId = clientId;
+            FreelancerId = freelancerId;
+            TotalCost = totalCost;
+
+            CreatedAt = DateTime.Now;
+            Comments = new List<ProjectComment>();
+            Status = ProjectStatusEnum.Created;
+        }
+
         public string Title { get; private set; }
         public string Description { get; private set; }
-        public int ClientId { get; set; }
-        public int FreelancerId { get; set; }
-        public decimal TotalCost { get; set; }
+        public int ClientId { get; private set; }
+        public int FreelancerId { get;  private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
+
     }
 }
---
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DevFreela.Core.Repositories
+{
+    interface IProjectRepository
+    {
+    }
+}
