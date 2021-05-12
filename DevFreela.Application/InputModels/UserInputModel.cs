53e0cead2bf99aeabe262e27767338cfe99b106f
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-05-12 19:40:06 -0300

    Users' service at application level implemented

---
+++ DevFreela.Application/InputModels/UserInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DevFreela.Application.InputModels
+{
+    public class UserInputModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
